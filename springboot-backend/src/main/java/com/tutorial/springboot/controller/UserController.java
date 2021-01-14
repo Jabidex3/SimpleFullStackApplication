fcb7e7af7fc95f302d8920bdb278df58c4587556
Jabid Methun <jabidmethun@gmail.com>
2021-01-14 15:51:07 -0500

    Update UserController.java

--- springboot-backend/src/main/java/com/tutorial/springboot/controller/UserController.java
+++ springboot-backend/src/main/java/com/tutorial/springboot/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tutorial.springboot.model.User;
 import com.tutorial.springboot.repository.UserRepository;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("api/")
 public class UserController {
